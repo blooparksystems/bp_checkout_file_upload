459128d2e46a706c687c6e744176d50b2fe4b3e0
Benjamin Bachmann <bbachmann@bloopark.de>
2015-04-23 15:32:25 +0200

    inherited ir.attachment

--- models/sale.py
+++ models/sale.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp.models import TransientModel
+from openerp.models import Model
 from openerp import fields
 
 import logging
@@ -8,8 +9,20 @@ _logger = logging.getLogger(__name__)
 class sale_order(TransientModel):
     _inherit = 'sale.order'
 
-    file = fields.Binary(
-         help="This field holds the Files for the Sale Order",
-         string='Uploaded Files'
-    )
+    file = fields.One2many('ir.attachment', 'partner_id', string="Attachments")
+
+class document_file(Model):
+    _inherit = 'ir.attachment'
+
+    def create(self, cr, uid, vals, context=None):
+        if vals.get('partner_id', 0) != 0 and not (vals.get('res_id', False) and vals.get('res_model', False)):
+            vals['res_id'] = vals['partner_id']
+            vals['res_model'] = 'res.partner'
+        return super(document_file, self).create(cr, uid, vals, context)
+
+    def write(self, cr, uid, ids, vals, context=None):
+        if vals.get('partner_id', 0) != 0 and not (vals.get('res_id', False) and vals.get('res_model', False)):
+            vals['res_id'] = vals['partner_id']
+            vals['res_model'] = 'res.partner'
+        return super(document_file, self).write(cr, uid, ids, vals, context)
 
